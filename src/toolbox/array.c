630fb6a0caa0841fcf3b9781b2321e82f8b124e3
gintoki2000 <ngotrungaccor@gmail.com>
2020-11-28 19:54:03 +0700

    generic array

---
+++ src/toolbox/array.c
@@ -0,0 +1,66 @@
+#include <stdlib.h>
+#include <string.h>
+#include <toolbox/array.h>
+#define ARRAY_DEFAULT_CAP 16
+
+Array* _array_new(u32 el_size) { return array_init(malloc(sizeof(Array)), el_size); }
+
+void array_del(Array* arr)
+{
+  if (arr != NULL)
+  {
+    array_fini(arr);
+    free(arr);
+  }
+}
+
+Array* array_init(Array* self, u32 el_size)
+{
+  self->cnt = 0;
+  self->cap = ARRAY_DEFAULT_CAP;
+  self->el_size = el_size;
+  self->storage = malloc(sizeof(s8) * ARRAY_DEFAULT_CAP);
+  return self;
+}
+
+void* _array_addn(Array* self, u32 n)
+{
+  u32 ncap;
+
+  if (self->cnt == self->cap) {
+  	ncap = self->cap;
+  	while (ncap < n)
+    	ncap *= 2;
+  	array_encap(self, ncap);
+  }
+  s8* ret = _array_at(self, self->cnt);
+  self->cnt += n;
+  return ret;
+}
+
+void* _array_at(Array *self, u32 idx)
+{
+	return self->storage + (idx * self->el_size);
+}
+
+u32 array_rmv(Array *self, u32 idx)
+{
+	for (u32 i = idx; i < self->cnt - 1; ++i)
+		memcpy(_array_at(self, i), _array_at(self, i + 1), self->el_size);
+	return self->cnt--;
+}
+
+u32 array_qrmv(Array *self, u32 idx)
+{
+	if (idx != self->cnt - 1)
+		memcpy(_array_at(self, idx), _array_at(self, self->cnt - 1), self->el_size);	
+	return self->cnt--;
+}
+
+void array_encap(Array* self, u32 new_cap)
+{
+	if (new_cap <= self->cap)
+		return;
+	self->storage = realloc(self->storage, sizeof(s8) * new_cap);
+	self->cap = new_cap;
+}
--- src/toolbox/array.h
+++ src/toolbox/array.h
@@ -1,23 +1,29 @@
 #ifndef ARRAY_H
 #define ARRAY_H
-#include "types.h"
+#include <common.h>
 typedef struct Array
 {
   s8* storage;
   u32 cnt;
   u32 cap;
-  u32 element_size;
+  u32 el_size;
 } Array;
 
-Array* array_new();
-void   array_delete();
+#define array_new(_T) (_array_new(sizeof(_T)))
+Array* _array_new(u32 el_size);
+void   array_del(Array* arr);
 
-Array* array_init(Array* self);
-void   array_fini(Array* self);
+Array* array_init(Array* _self, u32 el_size);
+void   array_fini(Array* _self);
 
-void* _array_addn(Array* self, u32 n);
+void* _array_addn(Array* _self, u32 n);
+#define array_add(_T, _self, _el) ((*(_T*)_array_addn(_self, 1)) = _el)
 
-/**/
-#define array_at(self, idx, T) ((T*) _array_at(self, idx))
-void* _array_at(Array* self, u32 idx);
+u32 array_rmv(Array* _self, u32 idx);
+u32 array_qrmv(Array* _self, u32 idx);
+
+#define array_at(_T, _self, _idx) ((_T*)_array_at(_self, _idx))
+void* _array_at(Array* _self, u32 idx);
+
+void array_encap(Array* _self, u32 new_cap);
 #endif // ARRAY_H

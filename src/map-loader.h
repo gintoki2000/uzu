6a75e34cffbb34671f8a59ff47d963ec0e67a7a1
Ngô Thời Trung <ngotrungaccor@gmail.com>
2020-12-28 11:50:11 +0700

    xóa một số file ko cần thiết

--- src/map-loader.h
+++
@@ -1,5 +0,0 @@
-#ifndef MAP_LOADER_H
-#define MAP_LOADER_H
-#include <ecs/ecs.h>
-int load_map(const char* file, Ecs* ecs);
-#endif // MAP_LOADER_H
--- src/sound.h
+++
@@ -1,5 +0,0 @@
-#ifndef SOUND_H
-#define SOUND_H
-void play_music();
-void stop_music();
-#endif //SOUND_H
